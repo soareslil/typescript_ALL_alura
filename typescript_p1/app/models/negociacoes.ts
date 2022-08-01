3fc00e82ccd0e632def5b233f1d1e71e8184e4c1
Liliane Soares <78764415+soareslil@users.noreply.github.com>
2022-08-01 12:09:51 -0300

    typescript part 1

---
+++ typescript_p1/app/models/negociacoes.ts
@@ -0,0 +1,15 @@
+import { Negociacao } from "./negociacao";
+
+export class Negociacoes{
+    private negociacoes: Negociacao[] = [];
+
+    adiciona(negociacao: Negociacao){
+        this.negociacoes.push(negociacao);
+    }
+
+    lista(): readonly Negociacao[]{
+        return this.negociacoes;
+    }
+}
+
+const negociacoes = new Negociacoes();
